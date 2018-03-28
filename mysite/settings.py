12f708f43e26931a735c0f024e60070d580a81eb
Clinton Collins <clinton@allgamer.net>
2018-03-28 06:38:57 -0500

    Fixed admin static files not working.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'pet',
     'sorl.thumbnail',
     'accounts',
@@ -132,6 +133,7 @@ LOGGING = {
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
